48b096454de19d81aab6790d54392d07a86aaf50
Peter Graf <magnumpi@gmail.com>
2019-10-24 15:00:29 -0400

    Updating setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pynto-punkbrwstr",
+    name="pynto",
     version="0.1",
     author="Peter Graf",
     author_email="peter@pynto.tech",
