972a84a1287996a86fbd85f89302c887c2615980
Peter Graf <magnumpi@gmail.com>
2019-10-24 14:40:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pynto-punkbrwstr",
-    version="0.0.1",
+    version="0.1",
     author="Peter Graf",
-    author_email="magnumpi@gmail.com",
+    author_email="peter@pynto.tech",
     description="Data analysis using a concatenative paradigm",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/punkbrwstr/pynto",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3.6',
 )
